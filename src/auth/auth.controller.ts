6a99f2cb1ea4e54a10aab8e9867bf9600a17b1af
Mohamed Haki <Mohamedhaki70@gmail.com>
2024-12-01 22:24:30 +0100

    Ajout de AuthController pour gérer les endpoints d'authentification.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+// src/auth/auth.controller.ts
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignupDto, SigninDto } from './dto/auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('Register')
+  async signup(
+    @Body() signupDto: SignupDto,
+  ) {
+    return this.authService.signup(signupDto.email, signupDto.password, signupDto.username);
+  } 
+
+  @Post('Login')
+  async signin(
+    @Body() signinDto : SigninDto,
+  ) {
+    return this.authService.signin(signinDto.email, signinDto.password);    
+  }
+}
